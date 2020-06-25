43b3aa13e131d0c9be92d146648ed7e0e10eb4f6
ChronisG <thegamedevelopergame@gmail.com>
2020-06-25 01:35:20 +0100

    add custom port flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -18,10 +19,12 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.tpl = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.tpl.Execute(w, nil)
+	t.tpl.Execute(w, r)
 }
 
 func main() {
+	port := flag.String("port", ":3000", "Port of the application")
+	flag.Parse()
 	room := newRoom()
 
 	http.Handle("/", &templateHandler{filename: "chat.html"})
@@ -29,7 +32,8 @@ func main() {
 
 	go room.run() // start new room
 
-	if err := http.ListenAndServe(":4000", nil); err != nil {
+	log.Println("Server is up on Port: ", *port)
+	if err := http.ListenAndServe(*port, nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
