013ba33f10a577949d3b17a6cbbfc04f03e2c567
ChronisG <thegamedevelopergame@gmail.com>
2020-06-29 00:40:28 +0100

    trace update

--- trace/tracer.go
+++ trace/tracer.go
@@ -1,6 +1,23 @@
 package trace
 
+import (
+	"fmt"
+	"io"
+)
+
 type Tracer interface {
 	// The ...interface{} argument type means that Trace accepts zero or more arguments of any type
 	Trace(...interface{})
+}
+
+type tracer struct {
+	out io.Writer
+}
+func (t *tracer) Trace(a ...interface{}) {
+	fmt.Fprint(t.out, a...)
+	fmt.Fprintln(t.out)
+}
+
+func New(w io.Writer) Tracer{
+	return &tracer{out: w}
 }
